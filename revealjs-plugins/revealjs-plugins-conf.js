5c8408ca7e666ec58f09672e2bce3747f2dce0da
GitLab <gitlab@noreply.com>
2019-01-06 19:39:07 +0000

    Site checkin for project CG WM Tools

--- revealjs-plugins/revealjs-plugins-conf.js
+++ revealjs-plugins/revealjs-plugins-conf.js
@@ -1,3 +1,6 @@
+//
+// CHALKBOARD PLUGIN https://github.com/rajgoel/reveal.js-plugins/tree/master/chalkboard
+//
 chalkboard: { 
 	readOnly: false, // Configuation option allowing to prevent changes to existing drawings.
 	transition: 800, // Gives the duration (in milliseconds) of the transition for a slide change, so that the notes canvas is drawn after the transition is completed.
@@ -12,13 +15,17 @@ keyboard: {
 	     8: function() { RevealChalkboard.reset() },	// reset chalkboard data on current slide when 'BACKSPACE' is pressed
 	    68: function() { RevealChalkboard.download() },	// download recorded chalkboard drawing when 'd' is pressed
 },
+//
+// MENU PLUGIN https://github.com/denehyg/reveal.js-menu
+//
 menu: {
 	numbers: true, // Add slide numbers to the titles in the slide list.
-	titleSelector: 'h1, h2', // Specifies which slide elements will be used for generating the slide titles in the menu. 
+	titleSelector: 'h1, h2, div.title, caption.title, #toctitle', // Specifies which slide elements will be used for generating the slide titles in the menu. 
 	transitions: true, // Specifies if the transitions menu panel will be shown.
 	openButton: true, // Adds a menu button to the slides to open the menu panel.
 	openSlideNumber: false, // If 'true' allows the slide number in the presentation to open the menu panel
 	loadIcons: true, // By default the menu will load it's own font-awesome library icons
+	sticky: true, // If 'true', the sticky option will leave the menu open until it is explicitly closed
 	custom: [
 		{ title: 'Keys', icon: '<i class="fa fa-keyboard">', content: `
 		<ul class="slide-menu-items">
@@ -54,39 +61,40 @@ menu: {
 		</li></ul>` }
 	],
   	themes: // Specifies the themes that will be available in the themes menu panel. Set to 'true' to show the themes menu panel with the default themes list. 
-	  	   [ { name: 'dark', theme: 'themes/reveal-zenika-dark.css' },
-			 { name: 'light', theme: 'themes/reveal-zenika-light.css' },
-			 { name: 'algeria-timgad', theme: 'themes/reveal-zenika-algeria-timgad.css' },
-			 { name: 'argentina-perito-moreno', theme: 'themes/reveal-zenika-mountain.css' },
-			 { name: 'bali-river', theme: 'themes/reveal-zenika-bali.css' },
-			 { name: 'cambodia-relax', theme: 'themes/reveal-zenika-cambodia-relax.css' },
-			 { name: 'greece-architecture', theme: 'themes/reveal-zenika-grece-architecture.css' },
-			 { name: 'guadeloupe-pool', theme: 'themes/reveal-zenika-guadeloupe.css' },
-			 { name: 'iceland-blocks', theme: 'themes/reveal-zenika-iceland-blocks.css' },
-			 { name: 'iceland-waterfall-shirt', theme: 'themes/reveal-zenika-iceland-waterfall-shirt.css' },
-			 { name: 'iceland-waterfall-zenikanard', theme: 'themes/reveal-zenika-iceland-waterfall-zenikanard.css' },
-			 { name: 'iceland-white', theme: 'themes/reveal-zenika-iceland-white.css' },
-			 { name: 'japan-roe', theme: 'themes/reveal-zenika-japan-biche.css' },
-			 { name: 'japan-fujisan', theme: 'themes/reveal-zenika-japan-fujisan.css' },
-			 { name: 'japan-garden', theme: 'themes/reveal-zenika-japan-garden.css' },
-			 { name: 'mauricia-bridge', theme: 'themes/reveal-zenika-mauricia-bridge.css' },
-			 { name: 'ocean-boats', theme: 'themes/reveal-zenika-ocean-boats.css' },
-			 { name: 'patagonia', theme: 'themes/reveal-zenika-patagonia.css' },
-			 { name: 'pilat-back', theme: 'themes/reveal-zenika-pilat-back.css' },
-			 { name: 'pilat-black-bag', theme: 'themes/reveal-zenika-pilat-black-bag.css' },
-			 { name: 'pompei', theme: 'themes/reveal-zenika-pompei.css' },
-			 { name: 'rock-climbing', theme: 'themes/reveal-zenika-escalade.css' },
-			 { name: 'thailand-phang-nga-back', theme: 'themes/reveal-zenika-thailand-cheow-lan-back.css' },
-			 { name: 'thailand-phang-nga-shirt', theme: 'themes/reveal-zenika-thailand-cheow-lan-shirt.css' },
-			 { name: 'usa-city', theme: 'themes/reveal-zenika-usa-city.css' },
-			 { name: 'vietnam-flag', theme: 'themes/reveal-zenika-vietnam-flag.css' },
-			 { name: 'cambodia-sunset', theme: 'themes/reveal-zenika-cambodia-sunset.css' },
-			 { name: 'greece-beach', theme: 'themes/reveal-zenika-grece-beach.css' },
-			 { name: 'japan-roe-dark', theme: 'themes/reveal-zenika-japan-biche-dark.css' },
-			 { name: 'japan-shibuya', theme: 'themes/reveal-zenika-tokyo-shibuya.css' },
-			 { name: 'japan-street-old', theme: 'themes/reveal-zenika-japan-street-old.css' },
-			 { name: 'japan-table', theme: 'themes/reveal-zenika-japan-table.css' },
-			 { name: 'mauricia-hotel', theme: 'themes/reveal-zenika-mauricia-hotel.css' },
-			 { name: 'music-beatles', theme: 'themes/reveal-zenika-beatles.css' },
-			 { name: 'scuba-diving', theme: 'themes/reveal-zenika-scuba-diving.css' }]
+	  	   [ { name: 'dark', theme: 'themes/css/reveal-zenika-dark.css' },
+			 { name: 'light', theme: 'themes/css/reveal-zenika-light.css' },
+			 { name: 'algeria-timgad', theme: 'themes/css/reveal-zenika-algeria-timgad.css' },
+			 { name: 'argentina-perito-moreno', theme: 'themes/css/reveal-zenika-mountain.css' },
+			 { name: 'bali-river', theme: 'themes/css/reveal-zenika-bali.css' },
+			 { name: 'brest-sea', theme: 'themes/css/reveal-zenika-brest-sea.css' },
+			 { name: 'cambodia-relax', theme: 'themes/css/reveal-zenika-cambodia-relax.css' },
+			 { name: 'greece-architecture', theme: 'themes/css/reveal-zenika-grece-architecture.css' },
+			 { name: 'guadeloupe-pool', theme: 'themes/css/reveal-zenika-guadeloupe.css' },
+			 { name: 'iceland-blocks', theme: 'themes/css/reveal-zenika-iceland-blocks.css' },
+			 { name: 'iceland-waterfall-shirt', theme: 'themes/css/reveal-zenika-iceland-waterfall-shirt.css' },
+			 { name: 'iceland-waterfall-zenikanard', theme: 'themes/css/reveal-zenika-iceland-waterfall-zenikanard.css' },
+			 { name: 'iceland-white', theme: 'themes/css/reveal-zenika-iceland-white.css' },
+			 { name: 'japan-roe', theme: 'themes/css/reveal-zenika-japan-biche.css' },
+			 { name: 'japan-fujisan', theme: 'themes/css/reveal-zenika-japan-fujisan.css' },
+			 { name: 'japan-garden', theme: 'themes/css/reveal-zenika-japan-garden.css' },
+			 { name: 'mauricia-bridge', theme: 'themes/css/reveal-zenika-mauricia-bridge.css' },
+			 { name: 'ocean-boats', theme: 'themes/css/reveal-zenika-ocean-boats.css' },
+			 { name: 'patagonia', theme: 'themes/css/reveal-zenika-patagonia.css' },
+			 { name: 'pilat-back', theme: 'themes/css/reveal-zenika-pilat-back.css' },
+			 { name: 'pilat-black-bag', theme: 'themes/css/reveal-zenika-pilat-black-bag.css' },
+			 { name: 'pompei', theme: 'themes/css/reveal-zenika-pompei.css' },
+			 { name: 'rock-climbing', theme: 'themes/css/reveal-zenika-escalade.css' },
+			 { name: 'thailand-phang-nga-back', theme: 'themes/css/reveal-zenika-thailand-cheow-lan-back.css' },
+			 { name: 'thailand-phang-nga-shirt', theme: 'themes/css/reveal-zenika-thailand-cheow-lan-shirt.css' },
+			 { name: 'usa-city', theme: 'themes/css/reveal-zenika-usa-city.css' },
+			 { name: 'vietnam-flag', theme: 'themes/css/reveal-zenika-vietnam-flag.css' },
+			 { name: 'cambodia-sunset', theme: 'themes/css/reveal-zenika-cambodia-sunset.css' },
+			 { name: 'greece-beach', theme: 'themes/css/reveal-zenika-grece-beach.css' },
+			 { name: 'japan-roe-dark', theme: 'themes/css/reveal-zenika-japan-biche-dark.css' },
+			 { name: 'japan-shibuya', theme: 'themes/css/reveal-zenika-tokyo-shibuya.css' },
+			 { name: 'japan-street-old', theme: 'themes/css/reveal-zenika-japan-street-old.css' },
+			 { name: 'japan-table', theme: 'themes/css/reveal-zenika-japan-table.css' },
+			 { name: 'mauricia-hotel', theme: 'themes/css/reveal-zenika-mauricia-hotel.css' },
+			 { name: 'music-beatles', theme: 'themes/css/reveal-zenika-beatles.css' },
+			 { name: 'scuba-diving', theme: 'themes/css/reveal-zenika-scuba-diving.css' }]
 }
--- revealjs-plugins/revealjs-plugins.js
+++ revealjs-plugins/revealjs-plugins.js
@@ -1,2 +1,3 @@
 { src: 'revealjs-plugins/reveal.js-menu/menu.js' },
-{ src: 'revealjs-plugins/chalkboard/chalkboard.js' }
+{ src: 'revealjs-plugins/chalkboard/chalkboard.js' },
+{ src: 'revealjs-plugins/title-footer/title-footer.js', async: true, callback: function() { title_footer.initialize(); } }
